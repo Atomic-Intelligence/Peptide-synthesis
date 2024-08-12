5908022990cd6f2e33a1550c84b3d23a5ea76ea9
Vinko-Dragusica-Atomic <ai_015@atmc.ai>
2024-08-12 12:43:01 +0000

    repaired merge_and_save

--- src/data/data_processing.py
+++ src/data/data_processing.py
@@ -1,9 +1,7 @@
-from pathlib import Path
 from tqdm import tqdm
 import polars as pl
 import pandas as pd
 import numpy as np
-import os
 
 from src.data.data_models import Data, Processor
 
@@ -82,41 +80,3 @@ class HFProcessorForSynthetization(Processor):
             (pl.col(self.primary_key) / 1000).cast(pl.Int64)
         )
         return data
-
-    def merge_and_save(
-        self,
-        clinical_data_0: pl.DataFrame,
-        clinical_data_1: pl.DataFrame,
-        peptides_data_0: pl.DataFrame,
-        peptides_data_1: pl.DataFrame,
-        save_to: Path | None = None,
-    ) -> None:
-        """If `save_to` parameter is None, data will be saved to current working directory."""
-
-        clinical_data = pl.concat([clinical_data_0, clinical_data_1]).transpose(
-            include_header=True
-        )
-
-        peptides_data = pl.concat([peptides_data_0, peptides_data_1]).transpose(
-            include_header=True
-        )
-
-        if save_to is not None:
-            Path.mkdir(save_to, exist_ok=True)
-            clinical_data.write_csv(
-                f"{save_to}/synthetic_data_clinical.csv", include_header=False
-            )
-            peptides_data.write_csv(
-                f"{save_to}/synthetic_data_peptides.csv", include_header=False
-            )
-
-        else:
-            save_to = os.getcwd()
-            clinical_data.write_csv(
-                f"{save_to}/synthetic_data_clinical.csv", include_header=False
-            )
-            peptides_data.write_csv(
-                f"{save_to}/synthetic_data_peptides.csv", include_header=False
-            )
-
-        print(f"Data saved to: {save_to}.")
