a834856f4b7a25f04362baf836d944bf1c2a7a4c
filipmirkovi <filip.mirkovic@atmc.ai>
2025-08-06 14:53:04 +0000

    Toy Data Fix

--- src/data/make_toy_data.py
+++ src/data/make_toy_data.py
@@ -18,37 +18,42 @@ def make_toy_data(num_samples: int, num_peptide_cols: int = 30) -> pd.DataFrame:
         + [f"Peptide_{i+1}" for i in range(num_peptide_cols)]
     )
     columns.remove("idAuswertung")
-
+    numerical_columns = (
+        NUMERICAL_CLINICAL_COLUMNS
+        + TIME_TO_EVENT_COLUMNS
+        + [f"Peptide_{i+1}" for i in range(num_peptide_cols)]
+    )
+    numerical_columns.remove("idAuswertung")
+    categorical_columns = CATEGORICAL_CLINICAL_COLUMNS
     marginals = np.random.choice(
         [stats.gamma(2), stats.lognorm(s=1), stats.expon],
         replace=True,
         size=num_peptide_cols,
     )
-    num_numerical_columns = (
-        len(NUMERICAL_CLINICAL_COLUMNS) + len(TIME_TO_EVENT_COLUMNS) + num_peptide_cols
-    )
+
     corr = np.clip(
-        0.5 * np.random.randn(num_numerical_columns, num_numerical_columns),
+        0.5 * np.random.randn(len(numerical_columns), len(numerical_columns)),
         a_min=-1,
         a_max=1,
     )
     corr = np.dot(corr, corr.T)
     corr = corr / np.linalg.norm(corr)
 
-    corr[np.diag_indices(num_numerical_columns)] = np.ones(num_numerical_columns)
+    corr[np.diag_indices(len(numerical_columns))] = np.ones(len(numerical_columns))
     numerical_copula = CopulaDistribution(
         copula=GaussianCopula(corr=corr), marginals=marginals
     )
     numerical_data = numerical_copula.rvs(nobs=num_samples)
 
     categorical_data = np.random.randint(
-        low=0, high=2, size=(num_samples, len(CATEGORICAL_CLINICAL_COLUMNS) - 1)
-    )
-    data = np.concatenate([categorical_data, numerical_data], axis=1)
-    dataframe = pd.DataFrame(data=data, columns=columns)
+        low=0, high=2, size=(num_samples, len(CATEGORICAL_CLINICAL_COLUMNS))
+    ).astype(int)
+    cat_dataframe = pd.DataFrame(data=categorical_data, columns=categorical_columns)
+    num_dataframe = pd.DataFrame(data=numerical_data, columns=numerical_columns)
     event_type = np.random.choice(
         ["no_event", "ckd", "hf"], p=[0.6, 0.15, 0.25], size=num_samples
     )
+    dataframe = pd.concat([cat_dataframe, num_dataframe], axis=1)
     dataframe["event_type"] = event_type
     dataframe["idAuswertung"] = list(range(100, 100 + num_samples))
     return dataframe
